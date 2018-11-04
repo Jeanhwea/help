a9439d66ce763840d879193a533645451afd85c0
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-04 15:01:10 +0800

    [change] date

--- build-index.py
+++ build-index.py
@@ -9,26 +9,27 @@ article = 'article'
 
 def getinfo(dir):
   orgfiles = []
-  prefix = '#+TITLE:'
   for root, dirs, files in os.walk(dir):
     for file in files:
       if file.endswith('.org'):
         filepath = os.path.join(root, file)
-        update = time.strftime('<%Y-%m-%d %a>', time.localtime(os.path.getmtime(filepath)))
         title = ''
+        date = ''
         with open(filepath, 'r') as f:
           for line in f:
-            if line.startswith(prefix):
-              title = line.replace(prefix, '').strip()
-        orgfiles.append((filepath, title, update))
+            if line.startswith('#+TITLE:'):
+              title = line.replace('#+TITLE:', '').strip()
+            if line.startswith('#+DATE:'):
+              date = line.replace('#+DATE:', '').strip()
+        orgfiles.append((filepath, title, date))
   return orgfiles
 
 def genindex(infos):
   index = ''
   i = 1
-  for path, title, update in infos:
-    index += '{i}. [[{path}][{title} {update}]]\n'.format(
-      i=i, path=path, title=title, update=update
+  for path, title, date in infos:
+    index += '{i}. [[{path}][{title}]] {date}\n'.format(
+      i=i, path=path, title=title, date=date
     )
     i += 1
   return index
@@ -40,6 +41,7 @@ def writereadme(index):
       content += line
       if line.startswith('*'):
         break
+  content += '\n'
   content += index
   with open('readme.org', 'w') as f:
     f.write(content)
